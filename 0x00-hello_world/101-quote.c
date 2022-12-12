d0f4bad73fb6c293d473e2013fd318196ce74d80
Sunny670 <sunny.thanki13@gmail.com>
2022-12-12 11:12:28 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - prints error
  *
- * Return: 1
+ *main - prints to the standard error
+ *
+ *
+ *
+ *Return: one
+ *
  *
  */
 int main(void)
+
 {
-write(2, "and that piece of art is useful\" -Dora Kopar, 2015-10-19\n" 59);
-return (1);
+
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+			return (1);
 
 }
