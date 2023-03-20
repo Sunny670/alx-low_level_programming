e9b5f7ff2da81de3ca92ae50ec1b4f30bae047e9
Sunny670 <sunny.thanki13@gmail.com>
2023-03-20 10:30:43 -0700

    task 2

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdlib.h>
+#include <stdio.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,5 +23,23 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
 
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+#endif
