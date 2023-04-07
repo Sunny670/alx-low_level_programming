5d2758e66bde198ad49d0195309db2142128edbb
Sunny670 <sunny.thanki13@gmail.com>
2023-04-07 17:18:47 +0300

    task3

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * key_index -  Index of a key
+ * @key: Key for getting the index for
+ * @size: Size of hash table
+ *
+ * Return: index for the key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size); //call the hash_djb2 function 
+}
