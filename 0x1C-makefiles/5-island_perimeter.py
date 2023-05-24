e08d3255d1e962a9c7e4dd6441d0660f9dfc499f
Sunny670 <sunny.thanki13@gmail.com>
2023-05-24 07:53:30 -0700

    task5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Contains island perimeter measuring function."""
+
+
+def island_perimeter(grid):
+    """Returns perimeter of island.
+
+    Grid represents water by 0 and land by 1.
+    """
+    height = len(grid)
+    width = len(grid[0])
+    size = 0
+    edges = 0
+
+    for x in range(height):
+        for y in range(width):
+            if grid[x][y] == 1:
+                size += 1
+                if (j > 0 and grid[x][y - 1] == 1):
+                    edges += 1
+                if (x > 0 and grid[x - 1][y] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
+
