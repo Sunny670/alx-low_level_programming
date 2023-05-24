467123b5390d43c6d3d9961e26ae1bc14f17ba0a
Sunny670 <sunny.thanki13@gmail.com>
2023-05-24 11:29:48 -0700

    task5 debugged

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -16,9 +16,8 @@ def island_perimeter(grid):
         for y in range(width):
             if grid[x][y] == 1:
                 size += 1
-                if (j > 0 and grid[x][y - 1] == 1):
+                if (y > 0 and grid[x][y - 1] == 1):
                     edges += 1
                 if (x > 0 and grid[x - 1][y] == 1):
                     edges += 1
     return size * 4 - edges * 2
-
